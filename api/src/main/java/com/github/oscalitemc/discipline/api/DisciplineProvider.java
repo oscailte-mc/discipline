615a31a3fcbcaf0b171afa016bb9ddcf4244f699
darbyjack <admin@glaremasters.me>
2022-01-02 23:24:13 -0600

    Add licenser to the project

--- api/src/main/java/com/github/oscalitemc/discipline/api/DisciplineProvider.java
+++ api/src/main/java/com/github/oscalitemc/discipline/api/DisciplineProvider.java
@@ -1,3 +1,23 @@
+/*
+ * Discipline
+ *
+ * Copyright (c) 2022 RÃ­an Errity (Paradaux)
+ *                    Glare
+ *                    Oscailte / Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.github.oscalitemc.discipline.api;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -16,15 +36,15 @@ public class DisciplineProvider {
 
     private DisciplineProvider() { }
 
-    public static void register(Discipline discipline) {
+    public static void register(final Discipline discipline) {
         DisciplineProvider.discipline = discipline;
     }
 
     public static Discipline discipline() {
-        if (discipline == null) {
+        if (DisciplineProvider.discipline == null) {
             throw new IllegalStateException("Discipline isn't ready."); // Carbon and LuckPerms design go brr
         }
 
-        return discipline;
+        return DisciplineProvider.discipline;
     }
 }
